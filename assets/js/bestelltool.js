437b26ae21b1dc9e10c2d1d3089a2332a6d118dc
Richard Neumann <r.neumann@homeinfo.de>
2022-05-02 15:28:54 +0200

    Use html key

--- assets/js/bestelltool.js
+++ assets/js/bestelltool.js
@@ -453,7 +453,7 @@ function validateNewOrder (newOrder) {
     Swal.fire({
         icon: 'error',
         title: 'Fehlende Anhaben',
-        text: toHTMLList(issues).outerHTML
+        html: toHTMLList(issues)
     })
     return false;
 }
