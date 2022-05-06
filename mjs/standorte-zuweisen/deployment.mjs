79df1533b3ef43ce93c5c8c10b08f29667b0116e
Richard Neumann <r.neumann@homeinfo.de>
2022-05-06 16:41:48 +0200

    Use keyup for filtering

--- mjs/standorte-zuweisen/deployment.mjs
+++ mjs/standorte-zuweisen/deployment.mjs
@@ -133,7 +133,7 @@ export class Deployment {
 
 export function init () {
     return getDeployments().then(render).then(() => {
-        $('#find-deployment').change(render);
+        $('#find-deployment').keyup(render);
     });
 }
 
