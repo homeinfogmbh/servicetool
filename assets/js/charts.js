46c35206dd6d71bbd12134bbf9ddabeb7581cb8f
Raphael Haupt <r.haupt@homeinfo.de>
2022-08-04 16:34:52 +0200

    edit chart

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,47 +1,79 @@
-var _hipsterIsOnline = true;
 $(document).ready(function() {
-    var xValues = [50,60,70,80,90,100,110,120,130,140,150];
-    var yValues = [7,8,8,9,9,9,10,11,14,14,15];
-    
-    new Chart("myChart", {
-      type: "line",
-      data: {
-        labels: xValues,
-        color:"rgba(255,0,255,1.0)",
-        datasets: [{
-          fill: false,
-          lineTension: 0,
-          pointRadius: 5,
-          backgroundColor: 'white',
-          borderColor: "rgba(255,130,29,1.0)",
-          data: yValues
-        }]
-      },
-      options: {
-        legend: {display: false},
-        scales: {
-          xAxes: [{
-            ticks: {fontColor:'white'},
-            gridLines: {
-              display: true,
-              color: 'white',
-              zeroLineColor: 'white',
-              zeroLineWidth: 3,
-            }
-          }],
-          yAxes: [{
-            ticks: {min: 0, max:16, fontColor:'white'},
-            gridLines: {
-              lineWidth: 0,
-              display: true,
-              //drawOnChartArea: false,
-              color: 'white',
-              zeroLineColor: 'white',
-              zeroLineWidth: 3,
-            }
-          }]
-        }
-      }
-    });
-    $("#pageloader").hide();
+  let values = getXYValues(300);
+
+  $("#days").change(function() {
+    let days = $("#days option:selected").val() === "allTime" ?values.xValues.length :parseInt($("#days option:selected").val());
+    //$("#canvas").remove();
+    $("#sysChart").html('<canvas id="canvas" style="width:100%; max-width:1000px; height:300px"></canvas>');
+    renderChart(values.xValues.slice(0, days), values.yValues.slice(0, days));
+  });
+
+  renderChart(values.xValues.slice(0, 30), values.yValues.slice(0, 30));
+  $("#pageloader").hide();
 });
+
+function getXYValues(days) {
+  let x = [];
+  let y = [];
+  for (let day = 1; day < days+1; day++) {
+    x.push(day);
+    y.push(Math.floor(Math.random() * 100) + 1);
+  }
+  return {"xValues":x, "yValues":y};
+}
+
+function renderChart(xValues, yValues) {
+  new Chart("canvas", {
+    type: "line",
+    data: {
+      labels: xValues,
+      color:"rgba(255,0,255,1.0)",
+      datasets: [{
+        fill: false,
+        lineTension: 0,
+        pointRadius: 5,
+        backgroundColor: 'white',
+        borderColor: "rgba(255,130,29,1.0)",
+        data: yValues
+      }]
+    },
+    options: {
+      legend: {display: false},
+      scales: {
+        xAxes: [{
+          ticks: {fontColor:'white'},
+          gridLines: {
+            display: true,
+            color: 'white',
+            zeroLineColor: 'white',
+            zeroLineWidth: 3,
+          },
+          scaleLabel: {
+            display: true,
+            color:'white',
+            labelString: 'Tage',
+            fontColor:'white',
+            fontSize: 20
+          }
+        }],
+        yAxes: [{
+          ticks: {fontColor:'white'},
+          gridLines: {
+            lineWidth: 0,
+            display: true,
+            //drawOnChartArea: false,
+            color: 'white',
+            zeroLineColor: 'white',
+            zeroLineWidth: 3,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Offline',
+            fontColor:'white',
+            fontSize: 20
+          }
+        }]
+      }
+    }
+  });
+}
