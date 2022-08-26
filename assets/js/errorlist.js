2fe2cfcbca861e9dc6a71f769b7802fab4d6594d
Raphael Haupt <r.haupt@homeinfo.de>
2022-08-26 13:46:35 +0200

    added operation system in list

--- assets/js/errorlist.js
+++ assets/js/errorlist.js
@@ -102,7 +102,19 @@ function setList(sort = "sortcustomer") {
                     '<td><span class="' + (check.hasOwnProperty("checkResults") && check.checkResults.length > 0 && (check.checkResults[0].applicationState === "conflict" || check.checkResults[0].applicationState === "not enabled" || check.checkResults[0].applicationState === "not running") ?"orangeCircle":"blueCircle") + '"></span></td>' +
                     '<td><span class="' + (check.hasOwnProperty("checkResults") && check.checkResults.length > 0 && check.checkResults[0].hasOwnProperty("applicationVersion") && _applicationVersion === check.checkResults[0].applicationVersion ?"blueCircle":"orangeCircle") + '"></span></td>' +
                     '<td>' + (check.hasOwnProperty("lastSync") ?formatDate(check.lastSync) + " (" + check.lastSync.substring(11, 16) + "h)": "noch nie") + '</td>' +
-                    '<td><i class="fa-linux"></i><a href="display-details.html?id=' + check.id + '" class="huntinglink"><img src="assets/img/circle-right.svg" alt="huntinglink"></a></td>' +
+                    '<td>' + check.operatingSystem + '</td>' +
+                    '<td><a href="display-details.html?id=' + check.id + '" class="huntinglink"><img src="assets/img/circle-right.svg" alt="huntinglink"></a></td>' +
+                    /*
+                    if (check.operatingSystem.indexOf(") === "Arch Linux")
+                        systemlistDOM += '<td><img src="assets/img/archlinux.png" title="' + check.operatingSystem + '"></td>';
+                    else if (check.operatingSystem === "Windows 10")
+                        systemlistDOM += '<td><img src="assets/img/archlinux.png" title="' + check.operatingSystem + '"></td>';
+                    else if (false)
+                        systemlistDOM += '<td><img src="assets/img/archlinux.png" title="' + check.operatingSystem + '"></td>';
+                    else
+                        systemlistDOM += '<td></td>';
+                    systemlistDOM += '<td><a href="display-details.html?id=' + check.id + '" class="huntinglink"><img src="assets/img/circle-right.svg" alt="huntinglink"></a></td>' +
+                    */
                 '</tr>';
                 counter++;
             }
