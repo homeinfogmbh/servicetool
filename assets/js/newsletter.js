5f56b2f9b88011f5d845e6286b905df2925b375f
Raphael Haupt <r.haupt@homeinfo.de>
2023-09-20 13:36:09 +0200

    cahnged design

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -5,10 +5,14 @@ $(document).ready(function() {
     $('.btn_addnewsletter').click(function(e) {
         let date = new Date(_newsletter[0].period);
         date.setMonth(date.getMonth()+1);
-        let period = date.getFullYear() + "-" + (date.getMonth()+1 < 10 ?"0"+date.getMonth()+1 :date.getMonth()+1) + "-" + (date.getDate() < 10 ?"0"+date.getDate() :date.getDate());
+        let period = date.getFullYear() + "-" + (date.getMonth()+1 < 10 ?"0" + (date.getMonth()+1) :date.getMonth()+1) + "-01";
         _newsletter.unshift({"id":-1, "subject":"", "text":"", "visible":false, "period":period});
-        setNewsletter();
-        $(".btn_newsletter").eq(0).click();
+        saveNewsletter(_newsletter[0].id, _newsletter[0].subject, _newsletter[0].text, _newsletter[0].visible, _newsletter[0].period).then((data) => {
+            if (_newsletter[0].id == -1)
+                _newsletter[0].id = data.id;
+            setNewsletter();
+        });
+        //$(".btn_newsletter").eq(0).click();
 		e.preventDefault();
 	});
 });
@@ -17,47 +21,55 @@ function setNewsletter(newsletters) {
     if (_newsletter === null) {
         _newsletter = newsletters.reverse();
     }
-    console.log(_newsletter)
     let newsletterDOM = ""
     let date;
     let newslettercounter = 0;
     for (let newsletter of _newsletter) {
         date = new Date(newsletter.period);
-        newsletterDOM += '<div class="tableBox BeobachtTable">' +
-            '<h3 style="color:' + (newsletter.visible ?"" :"#ff821d") + '"><u><i>' + _MONTHS[date.getMonth()] + ' ' + date.getFullYear() + '</i></u>' +
-                ' <a href="#" class="editIcon btn_newsletter" title="Newsletter bearbeiten"><img src="assets/img/edit.svg" alt=""></a>' +
-            '</h3>' +
-            '<div id="newslettersaveclose" style="float:right; display:none">' +
-                '<span class="whiteMark btn_saveNewsletter pointer" data-period="' + newsletter.period + '" data-id="' + newslettercounter + '">Speichern</span>' +
-                '<span class="whiteMark btn_newsletter pointer">Abbrechen</span>' +
-            '</div>' +
-            
-            '<div id="newslettervisibleselect" class="select" style="display:none">' +
-                '<select name="visible" id="visible" class="basic-data">' +
-                    '<option value="false" ' + (!newsletter.visible ?'selected' :'') + '>Entwurf</option>' +
-                    '<option value="true" ' + (newsletter.visible ?'selected' :'') + '>Freigeschaltet</option>' +
-                '</select>' +
-                '<span class="selectArrow"></span>' +
-            '</div><br>' +
-
-            '<span id="newslettersubject">Betreff: <b>' + newsletter.subject + '</span></b>' +
-            '<div id="subjectfield" style="display:none">' +
-                '<div class="dualinp inpCol">' +
-                    '<input id="subjectInput" type="text" class="longInp basic-data" value="' + newsletter.subject + '">' +
+        newsletterDOM += '<tr>' +
+            '<td>' +
+                _MONTHS[date.getMonth()] + ' (' + date.getFullYear() + ') ' + (newsletter.default ?'<span class="whiteMark">DEFAULT</span>' :"") + '<br>' +
+                '<span id="newslettersubject" style="font-size:30px;"><b>' + newsletter.subject + '</span></b>' +
+                '<div id="subjectfield" style="display:none">' +
+                    '<div class="dualinp inpCol">' +
+                        '<input id="subjectInput" type="text" class="longInp basic-data" value="' + newsletter.subject + '">' +
+                    '</div>' +
                 '</div>' +
-            '</div><br>' +
 
-            '<span id="newslettertext">' + newsletter.text.replaceAll("&lt;","<").replaceAll("&gt;",">") + '</span>' +
-            '<div id="newslettertextfield" style="display:none">' +
-                '<div class="dualinp inpCol">' +
-                    '<textarea id="newsletterTextInput" class="longInp basic-data" style=resize:auto;">' + newsletter.text.replaceAll("&lt;","<").replaceAll("&gt;",">").replaceAll("<br>","\n") + '</textarea>' +
+                //'<span id="newslettertext">' + newsletter.text.replaceAll("&lt;","<").replaceAll("&gt;",">") + '</span>' +
+                '<div id="newslettertextfield" style="display:none">' +
+                    '<div class="dualinp inpCol">' +
+                        '<textarea id="newsletterTextInput" class="longInp basic-data" style=resize:auto;">' + newsletter.text.replaceAll("&lt;","<").replaceAll("&gt;",">").replaceAll("<br>","\n") + '</textarea>' +
+                    '</div>' +
                 '</div>' +
-            '</div>' +
-        '</div><br>';
+                '<div id="newslettersaveclose" style="display:none">' +
+                    '<span class="whiteMark btn_saveNewsletter pointer" style="margin:0 20px 0 0" data-period="' + newsletter.period + '" data-id="' + newslettercounter + '">Speichern</span>' +
+                    '<span class="whiteMark btn_newsletter pointer">Abbrechen</span>' +
+                '</div><br>' +
+            '</td>' +
+            '<td style="min-width:250px; text-align:right; vertical-align: top;">' +
+                '<span class="whiteMark btn_testNewsletter pointer" title="Verschickt eine E-Mail an diesen Account">Testmailing</span>' +
+                '<a href="#" class="editIcon btn_newsletter" style="padding:0 20px 0 20px" title="Newsletter bearbeiten"><img src="assets/img/edit.svg"></a>' +
+                '<input type="checkbox" style="display:none;" name="newslettervisible" id="newslettervisible' + newslettercounter + '" ' + (newsletter.visible ?"checked" :"") + '>' +
+                '<label for="newslettervisible' + newslettercounter + '"><span class="btn_visible checkboxStyle orangeCheckbox" data-id="' + newslettercounter + '"></span></label>' +
+            '</td>' +
+        '</tr>';
         newslettercounter++;
     }
     $('#newsletterdata').html(newsletterDOM);
 
+    $('.btn_testNewsletter').click(function(e) {
+        Swal.fire({
+			title: "Newsletter",
+			text: "Wurde NOCH NICHT verschickt",
+			showCancelButton: false,
+			confirmButtonColor: '#ff821d',
+			iconHtml: '<img src="assets/img/PopUp-Icon.png"></img>',
+			confirmButtonText: 'O.K.',
+			buttonsStyling: true
+		});
+	});    
+    
     $('.btn_newsletter').click(function(e) {
         if ($(this).parent().parent().find("#subjectfield").is(":visible")) {
             $(this).parent().parent().find("#newslettervisibleselect").hide();
@@ -65,10 +77,10 @@ function setNewsletter(newsletters) {
             $(this).parent().parent().find("#newslettertextfield").hide();
             $(this).parent().parent().find("#newslettersaveclose").hide();
             $(this).parent().parent().find("#newslettersubject").show();
-            $(this).parent().parent().find("#newslettertext").show();
+            //$(this).parent().parent().find("#newslettertext").show();
         } else {
             $(this).parent().parent().find("#newslettersubject").hide();
-            $(this).parent().parent().find("#newslettertext").hide();
+            //$(this).parent().parent().find("#newslettertext").hide();
             $(this).parent().parent().find("#newslettervisibleselect").show();
             $(this).parent().parent().find("#subjectfield").show();
             $(this).parent().parent().find("#newslettertextfield").show();
@@ -76,22 +88,26 @@ function setNewsletter(newsletters) {
         }
 		e.preventDefault();
 	});
+
+    $('.btn_visible').click(function(e) {
+        _newsletter[$(this).data("id")].visible = !_newsletter[$(this).data("id")].visible;
+        saveNewsletter(_newsletter[$(this).data("id")].id, _newsletter[$(this).data("id")].subject, _newsletter[$(this).data("id")].text, _newsletter[$(this).data("id")].visible, _newsletter[$(this).data("id")].period).then((data) => {
+            $("#pageloader").hide();
+        });
+	});
+
     $('.btn_saveNewsletter').click(function(e) {
-        $("#pageloader").show();
         let id = _newsletter[$(this).data("id")].id;
         let subject = $(this).parent().parent().find('#subjectInput').val();
         let text = $(this).parent().parent().find('#newsletterTextInput').val().replace(/\n/g, "<br>");
-        let visible = $(this).parent().parent().find('#visible :selected').val();
+        let visible = $(this).parent().parent().find('#visible :selected').val() == "true";
         let period = $(this).data("period");
         _newsletter[$(this).data("id")].subject = subject;
         _newsletter[$(this).data("id")].text = text;
         _newsletter[$(this).data("id")].visible = visible;
         _newsletter[$(this).data("id")].period = period;
-        saveNewsletter(_newsletter[$(this).data("id")].id, subject, text, visible, period).then((data) => {
-            if (id == -1)
-                _newsletter[0].id = data.id;
+        saveNewsletter(id, subject, text, visible, period).then((data) => {
             setNewsletter();
-            $("#pageloader").hide()
         });
 		e.preventDefault();
 	});
@@ -109,6 +125,7 @@ function getNewsletter() {
 	});	
 }
 function saveNewsletter(id, subject, text, visible, period) {
+    $("#pageloader").show();
     let data = '{"subject":"' + subject + '","text":"' + text + '","visible":'+ visible + ', "period":"' + period + '"}';
     let url = "https://sysmon.homeinfo.de/patch_newsletter/" + id; 
     if (id == -1)
